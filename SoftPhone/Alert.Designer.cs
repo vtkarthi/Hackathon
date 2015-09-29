269e870cbfa88e8b9f0ef70b5d7d5d9d24884977
Godson <vtkarthicse@gmail.com>
2015-09-29 11:16:50 -0700

    softphone-godson

--- SoftPhone/Alert.Designer.cs
+++ SoftPhone/Alert.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SoftPhone
 {
-    partial class Form1
+    partial class Alert
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alert));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Test";
-            this.button1.UseVisualStyleBackColor = true;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // Form1
+            // Alert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Alert";
+            this.Text = "Alert";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
-
---
+++ SoftPhone/Caller.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SoftPhone
+{
+    partial class Caller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMakeCall = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCustomerNo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMakeCall
+            // 
+            this.btnMakeCall.Location = new System.Drawing.Point(96, 115);
+            this.btnMakeCall.Name = "btnMakeCall";
+            this.btnMakeCall.Size = new System.Drawing.Size(75, 23);
+            this.btnMakeCall.TabIndex = 0;
+            this.btnMakeCall.Text = "Make Call";
+            this.btnMakeCall.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblCustomerNo
+            // 
+            this.lblCustomerNo.AutoSize = true;
+            this.lblCustomerNo.Location = new System.Drawing.Point(52, 53);
+            this.lblCustomerNo.Name = "lblCustomerNo";
+            this.lblCustomerNo.Size = new System.Drawing.Size(125, 13);
+            this.lblCustomerNo.TabIndex = 2;
+            this.lblCustomerNo.Text = "Customer Phone Number";
+            // 
+            // Caller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 169);
+            this.Controls.Add(this.lblCustomerNo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnMakeCall);
+            this.Name = "Caller";
+            this.Text = "Customer Phone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMakeCall;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCustomerNo;
+    }
+}
