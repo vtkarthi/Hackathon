21133b7af6a5aff6fc6192cf427d1cc42dc5e3fa
Karthi T <vtkarthicse@gmail.com>
2015-09-29 10:07:28 -0700

    rename

--- MobileCoach/Controllers/mcController.cs
+++ MobileCoach/Controllers/mcController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace MobileCoach.Controllers
+namespace VzSoftphone.Controllers
 {
     public class mcController : ApiController
     {
