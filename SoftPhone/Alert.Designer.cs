ca1946c6ba480523d041b65b31bd9a16067486cf
Karthi T <karthi.thangavel@verizon.com>
2015-09-30 02:36:39 -0700

    Softphone changes

--- SoftPhone/Alert.Designer.cs
+++ SoftPhone/Alert.Designer.cs
@@ -30,34 +30,77 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alert));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblonhold = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAlertMessage = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 16);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 99);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lblonhold
+            // 
+            this.lblonhold.AutoSize = true;
+            this.lblonhold.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblonhold.Location = new System.Drawing.Point(90, 23);
+            this.lblonhold.Name = "lblonhold";
+            this.lblonhold.Size = new System.Drawing.Size(199, 38);
+            this.lblonhold.TabIndex = 1;
+            this.lblonhold.Text = "Call On Hold";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ALERT";
+            // 
+            // txtAlertMessage
+            // 
+            this.txtAlertMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAlertMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAlertMessage.Location = new System.Drawing.Point(11, 123);
+            this.txtAlertMessage.Multiline = true;
+            this.txtAlertMessage.Name = "txtAlertMessage";
+            this.txtAlertMessage.ReadOnly = true;
+            this.txtAlertMessage.Size = new System.Drawing.Size(268, 128);
+            this.txtAlertMessage.TabIndex = 3;
+            this.txtAlertMessage.Text = "Call on hold duration exceeded 40 seconds. Please swap the call to customer and e" +
+    "xtend the call hold !";
+            // 
             // Alert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtAlertMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblonhold);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Alert";
             this.Text = "Alert";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblonhold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAlertMessage;
     }
 }
--- SoftPhone/Caller.Designer.cs
+++ SoftPhone/Caller.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.btnMakeCall = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCustomerNo = new System.Windows.Forms.TextBox();
             this.lblCustomerNo = new System.Windows.Forms.Label();
+            this._totalElapsedTimeDisplay = new System.Windows.Forms.Label();
+            this.lblCallDuration = new System.Windows.Forms.Label();
+            this.btnEndCall = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMakeCall
@@ -41,13 +44,14 @@
             this.btnMakeCall.TabIndex = 0;
             this.btnMakeCall.Text = "Make Call";
             this.btnMakeCall.UseVisualStyleBackColor = true;
+            this.btnMakeCall.Click += new System.EventHandler(this.btnMakeCall_Click);
             // 
-            // textBox1
+            // txtCustomerNo
             // 
-            this.textBox1.Location = new System.Drawing.Point(55, 78);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(180, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtCustomerNo.Location = new System.Drawing.Point(55, 78);
+            this.txtCustomerNo.Name = "txtCustomerNo";
+            this.txtCustomerNo.Size = new System.Drawing.Size(180, 20);
+            this.txtCustomerNo.TabIndex = 1;
             // 
             // lblCustomerNo
             // 
@@ -58,16 +62,51 @@
             this.lblCustomerNo.TabIndex = 2;
             this.lblCustomerNo.Text = "Customer Phone Number";
             // 
+            // _totalElapsedTimeDisplay
+            // 
+            this._totalElapsedTimeDisplay.AutoSize = true;
+            this._totalElapsedTimeDisplay.Location = new System.Drawing.Point(80, 147);
+            this._totalElapsedTimeDisplay.Name = "_totalElapsedTimeDisplay";
+            this._totalElapsedTimeDisplay.Size = new System.Drawing.Size(55, 13);
+            this._totalElapsedTimeDisplay.TabIndex = 3;
+            this._totalElapsedTimeDisplay.Text = "00:00:000";
+            this._totalElapsedTimeDisplay.Visible = false;
+            // 
+            // lblCallDuration
+            // 
+            this.lblCallDuration.AutoSize = true;
+            this.lblCallDuration.Location = new System.Drawing.Point(12, 147);
+            this.lblCallDuration.Name = "lblCallDuration";
+            this.lblCallDuration.Size = new System.Drawing.Size(70, 13);
+            this.lblCallDuration.TabIndex = 4;
+            this.lblCallDuration.Text = "Call Duration:";
+            this.lblCallDuration.Visible = false;
+            // 
+            // btnEndCall
+            // 
+            this.btnEndCall.Location = new System.Drawing.Point(96, 115);
+            this.btnEndCall.Name = "btnEndCall";
+            this.btnEndCall.Size = new System.Drawing.Size(75, 23);
+            this.btnEndCall.TabIndex = 5;
+            this.btnEndCall.Text = "End Call";
+            this.btnEndCall.UseVisualStyleBackColor = true;
+            this.btnEndCall.Visible = false;
+            this.btnEndCall.Click += new System.EventHandler(this.btnEndCall_Click);
+            // 
             // Caller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(286, 169);
+            this.Controls.Add(this.btnEndCall);
+            this.Controls.Add(this.lblCallDuration);
+            this.Controls.Add(this._totalElapsedTimeDisplay);
             this.Controls.Add(this.lblCustomerNo);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCustomerNo);
             this.Controls.Add(this.btnMakeCall);
             this.Name = "Caller";
             this.Text = "Customer Phone";
+            this.Load += new System.EventHandler(this.Caller_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,7 +115,10 @@
         #endregion
 
         private System.Windows.Forms.Button btnMakeCall;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCustomerNo;
         private System.Windows.Forms.Label lblCustomerNo;
+        private System.Windows.Forms.Label _totalElapsedTimeDisplay;
+        private System.Windows.Forms.Label lblCallDuration;
+        private System.Windows.Forms.Button btnEndCall;
     }
 }
